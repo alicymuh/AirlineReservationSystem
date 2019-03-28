14aeb4e829d1a881c884ddfc5748099686ac50b8
Alice Muhimpundu (MINDTREE LIMITED) <v-almuh@microsoft.com>
2019-03-28 06:14:35 -0700

    flight reservation code

---
+++ AirlineReservation/AirlineReservation/Customer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+//Getters and setters
+void setFirstName(const std::string& firstName);
+const std::string& getFirstName() const;
+
+void setLastName(const std::string& lastName);
+const std::string& getLastName() const;
+
+void setBirthDate(const std::string& birthDate);
+const std::string& getBirthDate() const;
+
+void setEmail(const std::string& email);
+const std::string& getEmail() const;
+
+void setEmail(const std::string& email);
+const std::string& getEmail() const;
+
+void setPhoneNumber(const std::string& phoneNumber);
+const std::string& getphoneNumber() const;
+
+void setAddress(const std::string& address);
+const std::string& getAddress() const;
